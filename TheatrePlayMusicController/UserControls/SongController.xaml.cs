ee621ca2e1ab5299d4a1facc94321f623eb87717
Furkan Usul <71877385+Frext@users.noreply.github.com>
2022-03-02 20:54:40 +0300

    Add files via upload
    A music controller app I made for a theatre play at school.

---
+++ TheatrePlayMusicController/UserControls/SongController.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TheatrePlayMusicController.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SongController.xaml
+    /// </summary>
+    public partial class SongController : UserControl
+    {
+        public SongController()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            EnablePlayButton_DisablePauseButton();
+
+            volumeSlider.Value = 50;    // The volume element of MediaElement is set to 0.5 (50) by default. So, we should set the value of the slider to 50 by default.
+        }
+
+        public string SongName { get; set; }
+
+        public string MusicSource { get; set; }
+
+        #region Helper Methods
+
+        private void PlaySong()
+        {
+            myMusic.Play();
+        }
+
+        private void PauseSong()
+        {
+            myMusic.Pause();
+        }
+
+        private void EnablePlayButton_DisablePauseButton()
+        {
+            playSongButton.IsEnabled = true;
+            pauseSongButton.IsEnabled = false;
+        }
+
+        private void EnablePauseButton_DisablePlayButton()
+        {
+            playSongButton.IsEnabled = false;
+            pauseSongButton.IsEnabled = true;
+        }
+
+        private void SetBorderColorTo(Brush color)
+        {
+            myBorder.BorderBrush = color;
+        }
+        #endregion
+
+        private void playSongButton_Click(object sender, RoutedEventArgs e)
+        {
+            PlaySong();
+            SetBorderColorTo(Brushes.Red);
+            EnablePauseButton_DisablePlayButton();
+        }
+
+        private void pauseSongButton_Click(object sender, RoutedEventArgs e)
+        {
+            PauseSong();
+            SetBorderColorTo(Brushes.White);
+            EnablePlayButton_DisablePauseButton();
+        }
+
+        private void volumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            volumeTextBlock.Text = ((int)volumeSlider.Value).ToString(); // Update the text inside volume text block next to the volume slider.
+
+            myMusic.Volume = volumeSlider.Value / 100; // It's divided by 100 because the volume element uses a linear scale between 0 and 1.
+        }
+
+        private void restartSongButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Do you REALLY want to restart \"{songNameTextBlock.Text}\"?"  , "Application", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                myMusic.Stop();
+                SetBorderColorTo(Brushes.White);
+                EnablePlayButton_DisablePauseButton();
+            }
+
+        }
+    }
+}
