aebb0258e695312cc668a9db53cdf99aeb2e0638
Furkan Usul <71877385+Frext@users.noreply.github.com>
2022-03-09 22:32:59 +0300

    Added selectings songs with URI

--- TheatrePlayMusicController/MainWindow.xaml.cs
+++ TheatrePlayMusicController/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TheatrePlayMusicController
 {
@@ -23,6 +11,11 @@ namespace TheatrePlayMusicController
         public MainWindow()
         {
             InitializeComponent();
+
+            songController1.MusicSource = new Uri("Resources/Songs/BarMusic.m4a", UriKind.Relative);
+            songController2.MusicSource = new Uri("Resources/Songs/CarelessWhisper.mp3", UriKind.Relative);
+            songController3.MusicSource = new Uri("Resources/Songs/PhoneRingtone.mp3", UriKind.Relative);
+            songController4.MusicSource = new Uri(".", UriKind.Relative);
         }
     }
 }
