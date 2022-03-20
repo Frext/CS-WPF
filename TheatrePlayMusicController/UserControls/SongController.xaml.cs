b333f7fdab6ceea7abe0e1171eca5fdfba67d71b
Furkan Usul <71877385+Frext@users.noreply.github.com>
2022-03-20 15:14:52 +0300

    Added a timer and cleaned the code a bit

--- TheatrePlayMusicController/UserControls/SongController.xaml.cs
+++ TheatrePlayMusicController/UserControls/SongController.xaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -10,79 +11,150 @@ namespace TheatrePlayMusicController.UserControls
     /// </summary>
     public partial class SongController : UserControl
     {
+        private static class MyBorderColors
+        {
+            public static Brush InactiveColor = Brushes.White;
+            public static Brush ActiveColor = Brushes.Red;
+        }
+
         public SongController()
         {
             InitializeComponent();
-            this.DataContext = this;
+            DataContext = this;
 
             EnablePlayButton_DisablePauseButton();
 
-            volumeSlider.Value = 50;    // The volume element of MediaElement is set to 0.5 (50) by default. So, we should set the value of the slider to 50 by default.
+            sldVolumeValue.Value = 50;    // The volume element of MediaElement is set to 0.5 (50) by default. So, we should set the value of the slider to 50 by default.
+
+            // Pass the method that will be called in every 1000 milliseconds.
+            // Pass null for the state because we won't need it.
+            // The timer won'T start because it's delayed for an infinite time.
+            myTimer = new Timer(new TimerCallback(myTimer_Tick),null,Timeout.Infinite,1000);
         }
 
         public string SongName { get; set; }
 
         public Uri MusicSource { get; set; }
 
+        private readonly Timer myTimer;
+
         #region Helper Methods
 
-        private void PlaySong()
+        private void StartSong()
         {
-            myMusic.Play();
+            meSongPlayer.Play();
         }
 
         private void PauseSong()
         {
-            myMusic.Pause();
+            meSongPlayer.Pause();
+        }
+
+        private void ResetSong()
+        {
+            meSongPlayer.Stop();
         }
 
         private void EnablePlayButton_DisablePauseButton()
         {
-            playSongButton.IsEnabled = true;
-            pauseSongButton.IsEnabled = false;
+            btnStartSong.IsEnabled = true;
+            btnPauseSong.IsEnabled = false;
         }
 
         private void EnablePauseButton_DisablePlayButton()
         {
-            playSongButton.IsEnabled = false;
-            pauseSongButton.IsEnabled = true;
+            btnStartSong.IsEnabled = false;
+            btnPauseSong.IsEnabled = true;
         }
 
         private void SetBorderColorTo(Brush color)
         {
             myBorder.BorderBrush = color;
         }
+
+        private void StartTimer()
+        {
+            myTimer.Change(1, 1000);
+            // Start the timer with a little delay and set the interval to 1 second.
+
+            // The little delay is needed to obtain the position of the media player correctly. 
+            // If you set it to 0, and click on the buttons with this sequence : Start-Pause-Start, the text block shows 00:00 for one second, and shows the real position after one second.
+        }
+
+        private void PauseTimer()
+        {
+            myTimer.Change(Timeout.Infinite,Timeout.Infinite);
+            // Start the timer with an Infinite delay. In other words, don't start it.
+        }
+
+        private void UpdateTBTimePassed()
+        {
+            tbTimePassed.Text = meSongPlayer.Position.ToString(@"mm\ \:\ ss");
+        }
         #endregion
 
-        private void playSongButton_Click(object sender, RoutedEventArgs e)
+        private void sldVolumeValue_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            PlaySong();
-            SetBorderColorTo(Brushes.Red);
+            tbVolumeValue.Text = ((int)sldVolumeValue.Value).ToString(); // Update the text inside volume text block next to the volume slider.
+
+            meSongPlayer.Volume = sldVolumeValue.Value / 100; // It's divided by 100 because the volume element uses a linear scale between 0 and 1.
+        }
+
+        private void btnStartSong_Click(object sender, RoutedEventArgs e)
+        {
+            StartSong();
+            StartTimer();
+
+            SetBorderColorTo(MyBorderColors.ActiveColor);
             EnablePauseButton_DisablePlayButton();
         }
 
-        private void pauseSongButton_Click(object sender, RoutedEventArgs e)
+        private void btnPauseSong_Click(object sender, RoutedEventArgs e)
         {
             PauseSong();
-            SetBorderColorTo(Brushes.White);
+            PauseTimer();
+
+            SetBorderColorTo(MyBorderColors.InactiveColor);
+            EnablePlayButton_DisablePauseButton();
+        }
+        private void btnResetSong_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Do you REALLY want to reset \"{SongName}\"?", "Application", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                ResetSong();
+                UpdateTBTimePassed();
+
+                SetBorderColorTo(MyBorderColors.InactiveColor);
+                EnablePlayButton_DisablePauseButton();
+            }
+        }
+
+        private void meSongPlayer_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            ResetSong();
+            UpdateTBTimePassed();
+
+            SetBorderColorTo(MyBorderColors.InactiveColor);
             EnablePlayButton_DisablePauseButton();
         }
 
-        private void volumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void myTimer_Tick(object state)
         {
-            volumeTextBlock.Text = ((int)volumeSlider.Value).ToString(); // Update the text inside volume text block next to the volume slider.
+            // Use Invoke() to access the UI thread outside of it.
+            // https://stackoverflow.com/questions/9732709/the-calling-thread-cannot-access-this-object-because-a-different-thread-owns-it/9732853#9732853
 
-            myMusic.Volume = volumeSlider.Value / 100; // It's divided by 100 because the volume element uses a linear scale between 0 and 1.
-        }
-
-        private void restartSongButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (MessageBox.Show($"Do you REALLY want to restart \"{songNameTextBlock.Text}\"?"  , "Application", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            Dispatcher.Invoke(() =>
             {
-                myMusic.Stop();
-                SetBorderColorTo(Brushes.White);
-                EnablePlayButton_DisablePauseButton();
-            }
+                if (meSongPlayer.Source != null)
+                {
+                    UpdateTBTimePassed();
+                }
+                else
+                {
+                    MessageBox.Show($"The song file cannot be loaded for \"{SongName}\".\nThe program is going to terminate.", "Error", MessageBoxButton.OK);
+                    Application.Current.Shutdown();
+                }
+            });
 
         }
     }
